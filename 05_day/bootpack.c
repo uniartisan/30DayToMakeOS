fcc19728e63bd68d3166df784bee2b8c9bafca1c
uniartisan <ricardo@uniartisan.com>
2019-09-25 23:17:59 +0800

    add explanatory note
     set up preliminary memory management

--- 05_day/bootpack.c
+++ 05_day/bootpack.c
@@ -38,13 +38,13 @@ struct BOOTINFO {
 	char* vram;
 };
 
-struct SEGMENT_DESCRIPTOR {
+struct SEGMENT_DESCRIPTOR {		//存放GDT 全局分段
 	short limit_low, base_low;
 	char base_mid, access_right;
 	char limit_high, base_high;
 };
 
-struct GATE_DESCRIPTOR {
+struct GATE_DESCRIPTOR {		//IDT
 	short offset_low, selector;
 	char dw_count, access_right;
 	short offset_high;
